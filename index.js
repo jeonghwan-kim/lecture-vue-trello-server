eee629ec4ae187a358bcc1797a9e8431d0f0b86b
김정환 <ej88ej@gmail.com>
2018-08-06 19:57:54 +0900

    add sample board

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const PORT = process.env.PORT || 3000
 
 models.sequelize.sync({
   force: true
-}).then(_=> {
-  const user = models.User.build({
+}).then(async _=> {
+  const user = await models.User.create({
     email: 'test@test.com',
     password: '123123',
     name: 'Chris'
   })
-  user.save()
+  const board = await models.Board.create({
+    title: 'Sample Board',
+    userId: user.id
+  })
 }).then(_=> {
   app.listen(PORT, () => console.log(`server is running localhost:${PORT}`))
 })
