0bcbc80956ccb2a5f99e9c0e63ce6e78246c6391
Duckuism <rnejrl123456@gmail.com>
2020-03-12 13:02:50 +0900

    fix parameter type on next function

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use('/health', (_, res) => res.json({time: Date.now() - startT}))
 
 app.use((req, res, next) => {
   res.status = 404
-  next(Error('not found'))
+  next(() => Error('not found'))
 })
 
 app.use((err, req, res, next) => {
